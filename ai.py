28e088465c9ca161e42c817ff56a21c66b2c7935
Yaroslav Samchuk <samchukyaroslav@gmail.com>
2025-03-01 21:41:54 +0200

    Upgraded

--- ai.py
+++ ai.py
@@ -2,24 +2,29 @@ import g4f
 import g4f.Provider
 import os
 from telebot import TeleBot
+import time 
 
 response = g4f.ChatCompletion.create(
-    model=g4f.models.gemini_exp,
-    provider=g4f.Provider.PollinationsAI,
+    model=g4f.models.gemini_2_0_flash_thinking,
+    provider=g4f.Provider.Gemini,
     messages=[
         {"role" : "system", "content" : "You are not an AI and not an assistant, you are dansk teacher for ukrainian students with danish level B1-B2!"},
-        {"role" : "user", "content" : "Hi! Who are you and what can you do?"},
+        {"role" : "user", "content" : "Hej!"},
     ],
-    # stream=True
-    # cookies={
-    #     "__Secure-1PSID" : "g.a000uAjUAPGE2J6ekF4LhheHgMDw5GYKcCDVAwXpUuPwNxhTtnOSKNjcq94JDydHwEv_6944DwACgYKAQ4SARcSFQHGX2Mi8HhBsDLzBOqq2SpNhqNE5BoVAUF8yKrQhP7gITbDFCO7DgnbQa9n0076"
-    # }
+    # stream=True,
+    # api_key="AIzaSyB_wYlmOsTD9b2U84tdRgH9AD1nToHu_vg",
+    cookies={
+        "__Secure-1PSID" : "g.a000uAjUAPGE2J6ekF4LhheHgMDw5GYKcCDVAwXpUuPwNxhTtnOSKNjcq94JDydHwEv_6944DwACgYKAQ4SARcSFQHGX2Mi8HhBsDLzBOqq2SpNhqNE5BoVAUF8yKrQhP7gITbDFCO7DgnbQa9n0076",
+        "__Secure-1PSIDTS" : "sidts-CjEBEJ3XVzFUPJvJ2tVPJI0vq0f93Pi0RzyEtQK4tB4A4vUntGyXNPswjiIUxm1GLqjQEAA",
+        "__Secure-1PSIDCC" : "AKEyXzXCg77ACIuB3DGQPD9akIWRsu4iVY6rxQUvSidOPVgQjkCsR1ukaEdNxQ9W4JwfihaMmrIi"
+    },
+    # _sid="g.a000uAjUAPGE2J6ekF4LhheHgMDw5GYKcCDVAwXpUuPwNxhTtnOS95kMQAM2DzsE63sq3Nm3SwACgYKAXQSARcSFQHGX2MiH84kSHOAP7T6sqVswr13RxoVAUF8yKpCfVVMPq-B9BgjbLGRbPdc0076"
 )
 
 text = ""
 
 counter = 0
-for i in response:
+for i in response.split("\n\n\n\n\n\n")[1]:
     text += i
     if counter > 3:
         os.system("cls")
@@ -30,7 +35,10 @@ for i in response:
 os.system("cls")
 print(text)
 
-def ai_message(user_message, bot: TeleBot, messages_list):
+messages_list = {}
+
+def ai_message(user_message, bot: TeleBot, messages_list1):
+
     try:
         messages_list[str(user_message.chat.id)].append({"role" : "user", "content" : user_message.text})
     except Exception as e:
@@ -42,24 +50,31 @@ def ai_message(user_message, bot: TeleBot, messages_list):
     
     if len(user_messages) >= 1:
         response = g4f.ChatCompletion.create(
-            model=g4f.models.gemini_exp,
-            provider=g4f.Provider.PollinationsAI,
+            model=g4f.models.gemini_2_0_flash_thinking,
+            provider=g4f.Provider.Gemini,
             messages=[
                 {"role" : "system", "content" : "You are not an AI and not an assistant, you are dansk teacher for ukrainian students with danish level B1-B2!"},
             ] + user_messages,
             # stream=True
-            # cookies={
-            #     "__Secure-1PSID" : "g.a000uAjUAPGE2J6ekF4LhheHgMDw5GYKcCDVAwXpUuPwNxhTtnOSKNjcq94JDydHwEv_6944DwACgYKAQ4SARcSFQHGX2Mi8HhBsDLzBOqq2SpNhqNE5BoVAUF8yKrQhP7gITbDFCO7DgnbQa9n0076"
-            # }
+            cookies={
+                "__Secure-1PSID" : "g.a000uAjUAPGE2J6ekF4LhheHgMDw5GYKcCDVAwXpUuPwNxhTtnOSKNjcq94JDydHwEv_6944DwACgYKAQ4SARcSFQHGX2Mi8HhBsDLzBOqq2SpNhqNE5BoVAUF8yKrQhP7gITbDFCO7DgnbQa9n0076",
+                "__Secure-1PSIDTS" : "sidts-CjEBEJ3XVzFUPJvJ2tVPJI0vq0f93Pi0RzyEtQK4tB4A4vUntGyXNPswjiIUxm1GLqjQEAA",
+                "__Secure-1PSIDCC" : "AKEyXzXCg77ACIuB3DGQPD9akIWRsu4iVY6rxQUvSidOPVgQjkCsR1ukaEdNxQ9W4JwfihaMmrIi"
+            }
         )
 
         text = ""
         counter = 0
+
         bot_message = bot.send_message(user_message.chat.id, "...")
-        for i in response:
+        for i in response.split("\n\n\n\n\n\n")[1]:
             text += i
-            if counter > 10:
-                bot.edit_message_text(text, bot_message.chat.id, bot_message.id)
+            if counter > 100:
+                try:
+                    bot.edit_message_text(text, bot_message.chat.id, bot_message.id)
+                except:
+                    time.sleep(1)
+                    bot.edit_message_text(text, bot_message.chat.id, bot_message.id)
                 counter = 0
             counter += 1
         try:
@@ -67,4 +82,4 @@ def ai_message(user_message, bot: TeleBot, messages_list):
             bot.edit_message_text(text, bot_message.chat.id, bot_message.id)
         except Exception as err:
             print(err)
-        return 
+        return messages_list
